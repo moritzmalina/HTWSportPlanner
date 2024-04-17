e6ceff3c1598439a6539a6894b59e8e6565eafac
lawszki <d.ronski@icloud.com>
2024-04-17 18:33:06 +0200

    Push Test, keine Änderungen.

--- src/main/java/htwberlin/HTWSportPlanner/MyController.java
+++ src/main/java/htwberlin/HTWSportPlanner/MyController.java
@@ -9,16 +9,18 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 
+// :)
+
 @RestController
 public class MyController {
 
     @GetMapping("/entries")
     public List<PlannerEntries> greeting() {
-        PlannerEntries entry = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1,1), LocalDate.of(2024,1,1));
-        PlannerEntries entry1 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1,1), LocalDate.of(2024,1,1));
-        PlannerEntries entry2 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1,1), LocalDate.of(2024,1,1));
-        PlannerEntries entry3 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1,1), LocalDate.of(2024,1,1));
-        PlannerEntries entry4 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1,1), LocalDate.of(2024,1,1));
+        PlannerEntries entry = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry1 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry2 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry3 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry4 = new PlannerEntries("Test", "Test", "Monday", LocalTime.of(15, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
         return List.of(entry, entry1, entry2, entry3, entry4);
     }
 
