6308b2ad69a8e5a712820c1075950db757462b2a
moritzmalina <80785467+moritzmalina@users.noreply.github.com>
2024-04-17 12:53:53 +0200

    Spring Initializr Push
    EInbindung des Spring Web Frameworks

---
+++ src/main/java/htwberlin/HTWSportPlanner/HtwSportPlannerApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.HTWSportPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtwSportPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtwSportPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/HTWSportPlanner/HtwSportPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.HTWSportPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtwSportPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
