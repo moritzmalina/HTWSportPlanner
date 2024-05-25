06cb597f5130f25516532b2dd0b90f245d27503d
moritzmalina <80785467+moritzmalina@users.noreply.github.com>
2024-05-25 18:53:37 +0200

    CrossOrigin implemented

--- src/main/java/htwberlin/HTWSportPlanner/MyController.java
+++ src/main/java/htwberlin/HTWSportPlanner/MyController.java
@@ -1,7 +1,9 @@
 package htwberlin.HTWSportPlanner;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.sql.Time;
 import java.time.LocalDate;
@@ -12,6 +14,7 @@ import java.util.List;
 // :)
 
 @RestController
+@CrossOrigin(origins = "https://htwsportplannerfrontend.onrender.com/")
 public class MyController {
 
     @GetMapping("/entries")
