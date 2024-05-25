096a50758a3ac447c3da0d9dbf8a565bd5e38b28
moritzmalina <80785467+moritzmalina@users.noreply.github.com>
2024-05-25 19:57:15 +0200

    entries corrected

--- src/main/java/htwberlin/HTWSportPlanner/MyController.java
+++ src/main/java/htwberlin/HTWSportPlanner/MyController.java
@@ -19,11 +19,11 @@ public class MyController {
 
     @GetMapping("/entries")
     public List<PlannerEntries> greeting() {
-        PlannerEntries entry = new PlannerEntries("Kickboxen", "Ingo", "Montag", "Treskowallee",LocalTime.of(15, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
-        PlannerEntries entry1 = new PlannerEntries("Badminton", "Farhan", "Dienstag", "Treskowallee", LocalTime.of(11, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
-        PlannerEntries entry2 = new PlannerEntries("Cha Cha", "Adnan", "Freitag", "Treskowallee",LocalTime.of(19, 00), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
-        PlannerEntries entry3 = new PlannerEntries("Karate", "Dominik", "Mittwoch","Treskowallee", LocalTime.of(20, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
-        PlannerEntries entry4 = new PlannerEntries("Bouldern", "Moritz", "Donnerstag","Treskowallee", LocalTime.of(13, 00), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry = new PlannerEntries("Kickboxen", "Ingo", "Treskowallee","Montag", LocalTime.of(15, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry1 = new PlannerEntries("Badminton", "Farhan", "Treskowallee","Dienstag",  LocalTime.of(11, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry2 = new PlannerEntries("Cha Cha", "Adnan", "Treskowallee","Freitag", LocalTime.of(19, 00), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry3 = new PlannerEntries("Karate", "Dominik", "Treskowallee","Mittwoch", LocalTime.of(20, 30), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
+        PlannerEntries entry4 = new PlannerEntries("Bouldern", "Moritz", "Treskowallee","Donnerstag", LocalTime.of(13, 00), LocalDate.of(2024, 1, 1), LocalDate.of(2024, 1, 1));
         return List.of(entry, entry1, entry2, entry3, entry4);
     }
 
