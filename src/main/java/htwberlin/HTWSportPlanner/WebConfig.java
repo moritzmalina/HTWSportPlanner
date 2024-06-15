c2c6dd28f854f57747bf363ddde8e9d7b39453f4
moritzmalina <80785467+moritzmalina@users.noreply.github.com>
2024-06-15 17:33:34 +0200

    update WebConfig

--- src/main/java/htwberlin/HTWSportPlanner/WebConfig.java
+++ src/main/java/htwberlin/HTWSportPlanner/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(allowedOrigins.split(","))
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS");
             }
         };
     }
